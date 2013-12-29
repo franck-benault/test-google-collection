3f748f402935750c29f37ceee81c8f27fe0d3b08
franck-benault <franck-benault@orange.fr>
2013-12-29 18:12:22 +0100

    add junit dependency

--- sample1/src/main/java/net/franckbenault/guava/sample/Person.java
+++ sample1/src/main/java/net/franckbenault/guava/sample/Person.java
@@ -2,4 +2,18 @@ package net.franckbenault.guava.sample;
 
 public class Person {
 
+	private Integer id;
+	private String firstname;
+	
+	
+	
+	public int hasCode() {
+		return 1;
+	}
+	
+	public boolean equals(Object obj) {
+		
+		return true;
+	}
+	
 }
