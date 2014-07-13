90074915122f178079737c47f1564f373099dbbe
franck-benault <franck-benault@orange.fr>
2014-07-13 18:09:55 +0200

    add Guava stopwatch

---
+++ performanceMesure/src/main/java/net/franckbenault/guava/sample/SampleWithStopWatch.java
@@ -0,0 +1,48 @@
+package net.franckbenault.guava.sample;
+
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Stopwatch;
+
+public class SampleWithStopWatch {
+
+
+	
+	private static void subProcess1() {
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private static void subProcess2() {
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static long longProcess() {
+		System.out.println("SampleWithStopWatch");
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		
+		subProcess1();
+		final long date2 = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+		System.out.println("subprocess1 "+date2);
+		
+		subProcess2();
+		final long date3 = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+		System.out.println("subprocess1 "+(date3-date2));
+		
+		return date3;
+		
+	}
+	
+}
