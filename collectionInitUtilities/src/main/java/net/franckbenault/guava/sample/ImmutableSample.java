1b5514a18c383dae10dfefc36cdd55a2a741fba2
franck-benault <franck-benault@orange.fr>
2014-07-18 23:57:39 +0200

    Add immutable test with Guava

--- collectionInitUtilities/src/main/java/net/franckbenault/guava/sample/ImmutableSample.java
+++ collectionInitUtilities/src/main/java/net/franckbenault/guava/sample/ImmutableSample.java
@@ -1,7 +1,24 @@
 package net.franckbenault.guava.sample;
 
+import java.util.Collections;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
 public class ImmutableSample {
 
 	
+	public List<String> colors = Lists.newArrayList("red", "white", "black", "blue", "green", "yellow");
 	
+	
+	public List<String> getJavaImmutable() {
+		
+		return Collections.unmodifiableList(colors);
+	}
+	
+	public List<String> getGuavaImmutable() {
+		
+		return ImmutableList.copyOf(colors);
+	}
 }
---
+++ collectionInitUtilities/src/test/java/net/franckbenault/guava/sample/ImmutableSampleTest.java
@@ -0,0 +1,50 @@
+package net.franckbenault.guava.sample;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class ImmutableSampleTest {
+
+	@Test
+	public void testGetJavaImmutable() {
+		ImmutableSample sample = new ImmutableSample();
+		List<String> immutable = sample.getJavaImmutable();
+		assertEquals(immutable.size(),6);
+
+		try {
+			immutable.add("new color");
+			fail("exception expected");
+		} catch (Exception e) {
+			//ok
+		}
+
+		//immutable is in fact a view of the initial list
+		//if the initial list changes
+		//the view changes
+		sample.colors.add("new color");
+		assertEquals(immutable.size(),7);
+
+	}
+
+	@Test
+	public void testGetGuavaImmutable() {
+		ImmutableSample sample = new ImmutableSample();
+		List<String> immutable = sample.getGuavaImmutable();
+		assertEquals(immutable.size(),6);
+		
+		try {
+			immutable.add("new color");
+			fail("exception expected");
+		} catch (Exception e) {
+			//ok
+		}
+		
+		sample.colors.add("new color");
+		assertEquals(immutable.size(),6);
+
+	}
+
+}
