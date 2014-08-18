f9a4de1340a27067991c07132fb513448804de45
franck-benault <franck-benault@orange.fr>
2014-08-18 22:54:36 +0200

    mod12 clean import

--- mod12-cache/src/test/java/net/franckbenault/guava/sample/CacheWithMapTest.java
+++ mod12-cache/src/test/java/net/franckbenault/guava/sample/CacheWithMapTest.java
@@ -2,9 +2,6 @@ package net.franckbenault.guava.sample;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
